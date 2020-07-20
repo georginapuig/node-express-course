8d113908331a6fe6587bb81eeccf3bc810003e39
georgina puig <georginaxcodes@gmail.com>
2020-07-20 00:43:50 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express'); // access to the express library by searching your node_modules for "express"
 const app = express(); // creates an instance of the express constructor, which we will name "app"  
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
   { name:'Mark' },
@@ -21,6 +24,28 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	});
+});
+
+// In order to make POST requests, first we need to include the "body-parser" library from our node_modules (included with express)
+app.post('/login',function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: 'password and username match!',
+      token: 'encrypted token goes here'
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'password and username do not match'
+    });
+  }
 });
 
 app.listen(8000,function(){
